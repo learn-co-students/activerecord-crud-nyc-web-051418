f61727eadd1fe8d7c3a14b9e3578e2a7602d1eba
github username <github email address>
2018-05-28 22:35:00 -0400

    Done.

---
+++ db/migrate/20180528231009_create_movies.rb
@@ -0,0 +1,13 @@
+# add columns for title ,release_date, director, lead, and in_theaters
+
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.datetime :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
+    end
+  end
+end
